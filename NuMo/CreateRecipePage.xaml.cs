9b6a54fc094c2f8d850940f463ea81f66feed95c
ColleenRothe <colleenrothe516@gmail.com>
2017-02-02 12:16:20 -0700

    Create CreateRecipePage.xaml.cs

---
+++ NuMo/CreateRecipePage.xaml.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace NuMo
+{
+	public partial class CreateRecipePage : ContentPage
+	{
+		public CreateRecipePage()
+		{
+			InitializeComponent();
+		}
+	}
+}
