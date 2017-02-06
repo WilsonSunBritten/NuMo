2a648fc18c0f9b9b9b6fa01e9aaf66dceaa64d8e
ColleenRothe <colleenrothe516@gmail.com>
2017-02-06 15:12:18 -0700

    Create DRIPage.xaml.cs

---
+++ NuMo/DRIPage.xaml.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace NuMo
+{
+	public partial class DRIPage : ContentPage
+	{
+		public DRIPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
