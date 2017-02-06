0772799e525d35f7a10d3a5c5d44dc8081392165
ColleenRothe <colleenrothe516@gmail.com>
2017-02-06 12:58:11 -0700

    Beginning Message

--- NuMo/MyDayPage.xaml.cs
+++ NuMo/MyDayPage.xaml.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+
 
 using Xamarin.Forms;
 
@@ -10,18 +11,47 @@ namespace NuMo
 		public MyDayPage()
 		{
 			InitializeComponent();
+
 		}
-        void OnSliderValueChanged(object sender, ValueChangedEventArgs args)
-        {
 
-        }
-        void OnButtonClicked(object sender, EventArgs args)
-        {
+		void OnSliderValueChanged(object sender, ValueChangedEventArgs args)
+		{
 
-        }
-        void switcher_Toggled(object sender, ToggledEventArgs e)
-        {
-//           label.Text = String.Format("Switch is now {0}", e.Value);
-        }
-    }
+		}
+		void OnButtonClicked(object sender, EventArgs args)
+		{
+
+		}
+		void switcher_Toggled(object sender, ToggledEventArgs e)
+		{
+			//           label.Text = String.Format("Switch is now {0}", e.Value);
+		}
+	
+
+		protected override async void OnAppearing()
+		{
+			//to only show the startup message the first time the app is opened
+			if (Application.Current.Properties.ContainsKey("start_up"))
+			{
+				Application.Current.Properties["start_up"] = "false";
+			}
+			else {
+
+				Application.Current.Properties["start_up"] = "true";
+
+				await DisplayAlert("Welcome to NuMo", "LONG WELCOME MESSAGE HERE" +
+				                   " NUMO IS THE BESTEST THING EVER YOU SHOULD USE IT" +
+				                   " BE HEALTHY AND LIVE FOREVER  " +
+				                   " OMEGA OMEGA FATTS. THIS IS SUCH A COOL APP\n"+
+							   "Please fill in user settings", "OK");
+
+
+				App.Current.MainPage = new SettingsPage();
+
+			}
+
+		}
+
+
+	}
 }
