988fcf8ca0860c0682cbfd8f1580a8170886ca1c
epennell <e-penn@hotmail.com>
2017-02-13 09:42:48 -0700

    Adjusted MyDayPage to have the buttons kind of work

--- NuMo/MyDayPage.xaml.cs
+++ NuMo/MyDayPage.xaml.cs
@@ -8,50 +8,59 @@ namespace NuMo
 {
 	public partial class MyDayPage : ContentPage
 	{
-		public MyDayPage()
+        int clickTotal = 0;
+
+        public MyDayPage()
 		{
 			InitializeComponent();
 
-		}
+        }
 
 		void OnSliderValueChanged(object sender, ValueChangedEventArgs args)
 		{
 
 		}
-		void OnButtonClicked(object sender, EventArgs args)
+		void OnItemsClicked(object sender, EventArgs args)
 		{
-
-		}
-		void switcher_Toggled(object sender, ToggledEventArgs e)
+            clickTotal += 1;
+            items.Text = String.Format("{0} button click{1}",
+                                       clickTotal, clickTotal == 1 ? "" : "s");
+        }
+        void OnNutrientsClicked(object sender, EventArgs args)
+        {
+            clickTotal += 1;
+            items.Text = String.Format("{0} button click{1}",
+                                       clickTotal, clickTotal == 1 ? "" : "s");
+        }
+        void switcher_Toggled(object sender, ToggledEventArgs e)
 		{
 			//           label.Text = String.Format("Switch is now {0}", e.Value);
 		}
-	
 
-		protected override async void OnAppearing()
-		{
-			//to only show the startup message the first time the app is opened
-			if (Application.Current.Properties.ContainsKey("start_up"))
-			{
-				Application.Current.Properties["start_up"] = "false";
-			}
-			else {
+        //  protected override async void onappearing()
+        //{
+        //	//to only show the startup message the first time the app is opened
+        //	if (application.current.properties.containskey("start_up"))
+        //	{
+        //		application.current.properties["start_up"] = "false";
+        //	}
+        //	else {
 
-				Application.Current.Properties["start_up"] = "true";
+        //		application.current.properties["start_up"] = "true";
 
-				await DisplayAlert("Welcome to NuMo", "LONG WELCOME MESSAGE HERE" +
-				                   " NUMO IS THE BESTEST THING EVER YOU SHOULD USE IT" +
-				                   " BE HEALTHY AND LIVE FOREVER  " +
-				                   " OMEGA OMEGA FATTS. THIS IS SUCH A COOL APP\n"+
-							   "Please fill in user settings", "OK");
-
-
-				App.Current.MainPage = new SettingsPage();
-
-			}
-
-		}
+        //		await displayalert("welcome to numo", "long welcome message here" +
+        //		                   " numo is the bestest thing ever you should use it" +
+        //		                   " be healthy and live forever  " +
+        //		                   " omega omega fatts. this is such a cool app\n"+
+        //					   "please fill in user settings", "ok");
 
 
-	}
+        //		app.current.mainpage = new settingspage();
+
+        //	}
+
+        //}
+
+
+    }
 }
